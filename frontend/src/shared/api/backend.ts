77bab0caefa6c658c03abb3ac499ba4da81f7b99
Phil <202443443+Philipp-0386@users.noreply.github.com>
2026-03-17 20:50:07 +0100

    chore: add function to call backend with local .env file containing url (closes #5)

---
+++ frontend/src/shared/api/backend.ts
@@ -0,0 +1,18 @@
+import "server-only"
+
+const BASE_URL = process.env.SPRING_API_BASE_URL;
+
+if (!BASE_URL) {
+  throw new Error("SPRING_API_BASE_URL is not defined");
+}
+//Server only function allowing calls to spring backend 
+export default async function backendFetch(path: string, init: RequestInit = {}): Promise<Response> {
+  return fetch(`${BASE_URL}${path}`, {
+    ...init,
+    headers: {
+      "Content-Type": "application/json",
+      ...(init.headers ?? {}),
+    },
+    cache: "no-store",
+  });
+}
