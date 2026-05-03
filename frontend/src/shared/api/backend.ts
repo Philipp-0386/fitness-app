a2cacd9682d7cda6432f122f8bbbcd3733586969
Phil <202443443+Philipp-0386@users.noreply.github.com>
2026-05-03 02:25:39 +0200

    chore: edit nextjs backendFetch call to spring allowing JWT's (#19)

--- frontend/src/shared/api/backend.ts
+++ frontend/src/shared/api/backend.ts
@@ -1,6 +1,7 @@
 import 'server-only';
 
 import { NetworkError } from '@/shared/api/errors/network-error';
+import { cookies } from 'next/headers';
 
 const BASE_URL = process.env.SPRING_API_BASE_URL;
 
@@ -12,11 +13,14 @@ export default async function backendFetch(
   path: string,
   init: RequestInit = {},
 ): Promise<Response> {
+  const cookieStore = cookies();
+  const cookieHeader = (await cookieStore).toString();
   try {
     return await fetch(`${BASE_URL}${path}`, {
       ...init,
       headers: {
         'Content-Type': 'application/json',
+        ...(cookieHeader ? { Cookie: cookieHeader } : {}),
         ...(init.headers ?? {}),
       },
       cache: 'no-store',
